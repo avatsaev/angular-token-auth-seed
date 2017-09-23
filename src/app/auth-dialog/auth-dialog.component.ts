645cb41278bf12eab7bd64d43935b9418744313d
Vatsaev Aslan <a.vatsaev@gmail.com>
2017-09-24 00:55:37 +0200

    feat: upgrade to angular 4

--- src/app/auth-dialog/auth-dialog.component.ts
+++ src/app/auth-dialog/auth-dialog.component.ts
@@ -29,7 +29,7 @@ export class AuthDialogComponent implements OnInit {
     if(e.signedUp)
       this.closeDialog();
     else{
-      alert(e.err.json()['errors]'['full_messages'][0])
+      alert(e.err.json()['errors']['full_messages'][0])
     }
   }
 
